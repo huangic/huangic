be90237086d06b8a05ebeb12c90df63d7ce41e4c
gendou915@gmail.com <gendou915@gmail.com@d39134a8-c9d7-11de-a70d-3b9dd4243769>
2011-03-11 05:40:13 +0000

    Extension

--- trunk/NXEIP/NXEIP/App_Code/Lib/DateUtil.cs
+++ trunk/NXEIP/NXEIP/App_Code/Lib/DateUtil.cs
@@ -6,7 +6,7 @@ using System.Web;
 /// <summary>
 /// DateUtil 的摘要描述
 /// </summary>
-public class DateUtil
+public static class DateUtil
 {
 	public DateUtil()
 	{
@@ -14,16 +14,24 @@ public class DateUtil
 		// TODO: 在此加入建構函式的程式碼
 		//
 	}
-
-    public static DateTime ConvertToZeroHour(DateTime dt) {
+    /// <summary>
+    /// 轉換成yyyy-MM-dd-00-00-00
+    /// </summary>
+    /// <param name="dt"></param>
+    /// <returns></returns>
+    public static DateTime ConvertToZeroHour(this DateTime dt) {
         //DateTime newDt = dt;
         String dt_str=dt.ToString("yyyy-MM-dd")+"-00-00-00";
 
         return DateTime.ParseExact(dt_str,"yyyy-MM-dd-HH-mm-ss",null);
 
     }
-
-    public static DateTime ConvertToMaxHout(DateTime dt)
+    /// <summary>
+    /// 轉換成yyyy-MM-dd-23-59-59
+    /// </summary>
+    /// <param name="dt"></param>
+    /// <returns></returns>
+    public static DateTime ConvertToMaxHour(this DateTime dt)
     {
         //DateTime newDt = dt;
         String dt_str = dt.ToString("yyyy-MM-dd") + "-23-59-59";
