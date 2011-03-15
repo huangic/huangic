c00c9226dab6bca771366e9ca7b3a061fa8e40ea
gendou915@gmail.com <gendou915@gmail.com@d39134a8-c9d7-11de-a70d-3b9dd4243769>
2011-03-15 05:40:06 +0000

    檔案區Widget

---
+++ NXEIP/NXEIP/widget/20/200100/200107-1.ascx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Entity;
+using System.Web.UI.HtmlControls;
+using System.Data.Objects.SqlClient;
+using NXEIP.DAO;
+
+public partial class widget_20_200100_200107_1 : NXEIP.Widget.WidgetBaseControl
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+         SessionObject sessionObj = new SessionObject();
+
+        string user_login = sessionObj.sessionUserAccount;
+        int peouid = int.Parse(sessionObj.sessionUserID);
+
+        //this.ObjectDataSource1.SelectParameters[0].DefaultValue = sessionObj.sessionUserDepartID;
+
+        this.DataList1.DataBind();
+
+
+    }
+
+    public override string Name
+    {
+        get { return "NewUnitFile"; }
+    }
+
+    public override void loadWidget()
+    {
+       
+    }
+
+    protected String GetS06Name(int s06no) {
+        using (NXEIPEntities model = new NXEIPEntities())
+        {
+            var cat = (from c in model.sys06 where c.s06_no == s06no select c).First();
+
+            if (cat.s06_level == 1)
+            {
+                return cat.s06_name;
+            }
+            else
+            {
+                var p_cat = (from c in model.sys06 where c.s06_no == cat.s06_parent select c).First();
+                return p_cat.s06_name;
+            }
+
+
+
+        }
+    
+    }
+}
